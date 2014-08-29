8687819e3ade0ab6539f13a42ee676a02127194a
jpttrindade <tenoriojpaulo@gmail.com>
2014-08-29 11:57:36 -0300

    dsfd

--- src/annotation/PrimaryKey.java
+++ src/annotation/PrimaryKey.java
@@ -2,6 +2,7 @@ package annotation;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+
 @Retention(RetentionPolicy.RUNTIME)
 public @interface PrimaryKey {
 	int ordem() default 0;
---
+++ src/models/ManyToMany.java
@@ -0,0 +1,9 @@
+package models;
+
+public class ManyToMany<T> {
+	
+	T param;
+	
+	
+
+}
